e93d39e0579f8a2aef86191bc3400123d9b2c82d
juan Flores <juanflorescenturion@gmail.com>
2023-04-17 18:51:36 -0500

    inicial

---
+++ src/main/java/com/fundamenosSpringBoot/fundamenos/FundamenosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamenosSpringBoot.fundamenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamenosApplication.class, args);
+	}
+
+}
